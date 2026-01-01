6565f8ace5d462bde99868146b3bd2b102877cf8
Fawaz Bello <96032614+Phawazz@users.noreply.github.com>
2026-01-01 23:57:45 +0100

    tg bot practice

---
+++ bot.py
@@ -0,0 +1,80 @@
+from typing import Final
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+
+
+TOKEN: Final = '8522170350:AAEkQx1OlHo3W3Wr2-_5r-P3IcZBU5MhDD4'
+BOT_USERNAME: Final = '@learn_practise_bot'
+
+# Commands
+async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Hello! Thanks for chatting with me. I'm the best tutor in the world.")
+    
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("I'm a tutor. What learning difficulties are you facing so I can help?")
+    
+async def custom_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("This is a custom command")
+    
+    
+# Responses
+
+def handle_response(text: str) -> str:
+    
+    processed: str = text.lower()
+    print(processed)
+    if 'hello' or 'Hi' in processed:
+        return 'Hey there!'
+    
+    if 'how are you' in processed:
+        return 'I am good'
+    
+    if 'i love python' in processed:
+        return 'Remember to subscribe'
+    
+    return 'I do not understand what you wrote'
+
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    message_type: str = update.message.chat.type
+    text: str = update.message.text
+    
+    print(f'User ({update.message.chat.id}) in {message_type}: "{text}"')
+    
+    if message_type == 'group':
+        if BOT_USERNAME in text:
+            new_text: str = text.replace(BOT_USERNAME, '').strip()
+            response: str = handle_response(new_text)
+        else:
+            return
+        
+    else:
+        response: str = handle_response(text)
+        
+    print('Bot:', response)
+    await update.message.reply_text(response)
+    
+
+async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print(f'Update {update} caused error {context.error}')
+    
+    
+if __name__ == '__main__':
+    print('Starting bot...')
+    app = Application.builder().token(TOKEN).build()
+    
+    # Commands
+    app.add_handler(CommandHandler('start', start_command))
+    app.add_handler(CommandHandler('help', help_command))
+    app.add_handler(CommandHandler('custom', custom_command))
+    
+    # Messages
+    app.add_handler(MessageHandler(filters.TEXT, handle_message))
+    
+    # Errors
+    app.add_error_handler(error)
+    
+    # Polls the bot
+    app.run_polling(poll_interval=3)
+    
+    
+    
